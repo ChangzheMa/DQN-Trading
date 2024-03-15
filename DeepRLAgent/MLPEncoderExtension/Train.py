d3a1a7239b9d2dcf9fde9fa7f67b19ff5572c14d
ChangzheMa <54517015+ChangzheMa@users.noreply.github.com>
2024-03-15 16:39:05 +0800

    add mymodel v2

--- DeepRLAgent/MLPEncoderExtension/Train.py
+++ DeepRLAgent/MLPEncoderExtension/Train.py
@@ -4,7 +4,6 @@ from DeepRLAgent.MLPEncoderExtension.Decoder import Decoder
 from DeepRLAgent.MLPEncoderExtension.Encoder import Encoder
 from DeepRLAgent.BaseTrain import BaseTrain
 import torch
-from torchsummary import summary
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
